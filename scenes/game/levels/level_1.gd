8b0e548226ab89f8be4c7dd24f4908366b99bdb5
Marek Belski <marek.belski@gmail.com>
2026-09-13 15:22:06 -0700

    Makes basic encroaching doom for level 1 and 2.

--- scenes/game/levels/level_1.gd
+++
@@ -1,7 +0,0 @@
-extends "level.gd"
-
-func _on_lose_button_pressed() -> void:
-	level_lost.emit()
-
-func _on_win_button_pressed() -> void:
-	level_won.emit(next_level_path)
--- scenes/game/levels/level_2d.gd
+++ scenes/game/levels/level_2d.gd
@@ -1,7 +1,12 @@
 extends "level.gd"
+@export var doom_speed = 20.0
+@onready var lose_area_2d = %LoseArea2D
 
 func _on_lose_area_2d_body_entered(_node: Node2D) -> void:
 	level_lost.emit()
 
 func _on_win_area_2d_body_entered(_node: Node2D) -> void:
 	level_won.emit(next_level_path)
+
+func _process(delta):
+	lose_area_2d.position.y -= delta * doom_speed
