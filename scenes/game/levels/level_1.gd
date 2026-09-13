df6798207b14e46651f34961803d66c0c48e44dd
Marek Belski <marek.belski@gmail.com>
2026-09-13 13:50:30 -0700

    Updates shader.

--- scenes/game/levels/level_1.gd
+++ scenes/game/levels/level_1.gd
@@ -5,13 +5,3 @@ func _on_lose_button_pressed() -> void:
 
 func _on_win_button_pressed() -> void:
 	level_won.emit(next_level_path)
-
-func _ready() -> void:
-	super._ready()
-	%ColorPickerButton.color = level_state.color
-	%BackgroundColor.color = level_state.color
-
-func _on_color_picker_button_color_changed(color : Color) -> void:
-	%BackgroundColor.color = color
-	level_state.color = color
-	GlobalState.save()
