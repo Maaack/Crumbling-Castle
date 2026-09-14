2296bb8b1d772b5eb41780656c4a1365d677842d
Marek Belski <marek.belski@gmail.com>
2026-09-14 14:00:02 -0700

    Adds gate wheel that turns as player interacts with it.

--- scenes/game/levels/gate_wheel.gd
+++ scenes/game/levels/gate_wheel.gd
@@ -1,19 +1,38 @@
 extends Node2D
 
+signal wheel_turned
+
 enum WheelState{
-	DIAGONAL,
 	CARDINAL,
+	DIAGONAL
 }
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var animation_player = %AnimationPlayer
 
+var wheel_state : WheelState = WheelState.CARDINAL
+var can_interact : bool = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func turn_wheel() -> void:
+	match wheel_state:
+		WheelState.CARDINAL:
+			wheel_state = WheelState.DIAGONAL
+			animation_player.play(&"diagonal")
+		WheelState.DIAGONAL:
+			wheel_state = WheelState.CARDINAL
+			animation_player.play(&"cardinal")
+	wheel_turned.emit()
 
 func _on_area_2d_body_entered(body : Node2D) -> void:
 	if body.is_in_group(&"player"):
-		print("can interact")
+		can_interact = true
+
+func _on_area_2d_body_exited(body : Node2D) -> void:
+	if body.is_in_group(&"player"):
+		can_interact = false
+
+func _input(event : InputEvent) -> void:
+	if not can_interact:
+		return
+	if event.is_action_pressed(&"interact"):
+		turn_wheel()
+		
