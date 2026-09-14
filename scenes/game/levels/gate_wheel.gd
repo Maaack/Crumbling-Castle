c14894d2985dc5c6a405231c8c15307ed8a1b1b7
Marek Belski <marek.belski@gmail.com>
2026-09-14 13:14:48 -0700

    Connect script to gate wheel.

--- scenes/game/levels/gate_wheel.gd
+++ scenes/game/levels/gate_wheel.gd
@@ -13,3 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_area_2d_body_entered(body : Node2D) -> void:
+	if body.is_in_group(&"player"):
+		print("can interact")
