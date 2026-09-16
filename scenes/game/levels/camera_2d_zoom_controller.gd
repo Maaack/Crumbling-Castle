afd1704a794ffaa9d87e25e1cf850912ea29368c
Marek Belski <marek.belski@gmail.com>
2026-09-16 10:13:32 -0700

    Even clamping the zoom to just zooming in still has the issue.

--- scenes/game/levels/camera_2d_zoom_controller.gd
+++ scenes/game/levels/camera_2d_zoom_controller.gd
@@ -1,8 +1,10 @@
 extends Node
 
 @export var camera : Camera2D
-@export var base_distance : float = 300
+@export var base_distance : float = 600
 @export var zoom_ratio : float = 0.0
+@export var min_zoom_ratio : float = -1.0
+@export var max_zoom_ratio : float = 1.0
 
 var initial_zoom : Vector2
 var doom_node : Node2D
@@ -19,5 +21,5 @@ func _ready() -> void:
 func _process(_delta):
 	var vertical_distance : float = abs(doom_node.global_position.y - camera.global_position.y)
 	var relative_distance : float = base_distance - vertical_distance
-	var relative_zoom : float = zoom_ratio * relative_distance
+	var relative_zoom : float = clamp(zoom_ratio * relative_distance, min_zoom_ratio, max_zoom_ratio)
 	camera.zoom = initial_zoom * (1 + relative_zoom)
