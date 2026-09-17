1c3769de8b95c84883f87db6caa647618b3be6fe
Marek Belski <marek.belski@gmail.com>
2026-09-16 22:23:40 -0700

    Has the dialogue start the trip and music.

--- scenes/game/scripts/level_shader_controller.gd
+++ scenes/game/scripts/level_shader_controller.gd
@@ -3,16 +3,12 @@ extends Node
 
 @export var level_loader : LevelLoader
 @export var canvas : CanvasItem
-@export var mod_parameters : Array[StringName]
-@export var mod_amount : float = 0.0
 
 var parameter_defaults : Dictionary[StringName, Variant]
 var shader_material : ShaderMaterial
 
 func _ready() -> void:
 	shader_material = canvas.material
-	for mod_parameter in mod_parameters:
-		parameter_defaults[mod_parameter] = shader_material.get_shader_parameter(mod_parameter)
 	level_loader.level_loaded.connect(_on_level_loaded)
 
 func _update_shader_parameter(value : Variant, key : StringName) -> void:
@@ -20,6 +16,14 @@ func _update_shader_parameter(value : Variant, key : StringName) -> void:
 
 func _on_level_loaded() -> void:
 	if level_loader.current_level_path.contains("level_0"):
+		shader_material.set_shader_parameter(&"opacity", 0.0)
 		shader_material.set_shader_parameter(&"original_opacity_mod", 0.95)
+		if level_loader.current_level.has_signal(&"trip_started"):
+			level_loader.current_level.connect(&"trip_started", _on_trip_started)
 	else:
+		shader_material.set_shader_parameter(&"opacity", 0.75)
 		shader_material.set_shader_parameter(&"original_opacity_mod", 0.666)
+
+func _on_trip_started() -> void:
+	var tween = create_tween()
+	tween.tween_method(_update_shader_parameter.bind(&"opacity"), 0.0, 0.75, 12.0)
