2eb7cb6098cb061db40079431b020fefd9ab31e7
Marek Belski <marek.belski@gmail.com>
2026-09-18 06:37:41 -0700

    Add turning wheel and lifting gate to 1D level.

--- scenes/game/levels/level_1d.gd
+++ scenes/game/levels/level_1d.gd
@@ -15,6 +15,13 @@ extends "level.gd"
 @onready var step_12 = %Step12
 @onready var step_13 = %Step13
 @onready var step_14 = %Step14
+@onready var step_15 = %Step15
+@onready var step_16 = %Step16
+@onready var step_17 = %Step17
+@onready var step_18 = %Step18
+@onready var step_19 = %Step19
+@onready var gate_situation_label = %GateSituationLabel
+@onready var gate_opening_button = %GateOpeningButton
 
 @onready var all_containers : Array[Node] = [
 	step,
@@ -31,6 +38,11 @@ extends "level.gd"
 	step_12,
 	step_13,
 	step_14,
+	step_15,
+	step_16,
+	step_17,
+	step_18,
+	step_19,
 ]
 @onready var step_order : Array[Node] = [
 	step,
@@ -85,6 +97,34 @@ extends "level.gd"
 	step_4,
 	step_5,
 	step_5,
+	step_6,
+	step_13,
+	step_14,
+	step_13,
+	step_14,
+	step_10,
+	step_11,
+	step_4,
+	step_5,
+	step_5,
+	step_6,
+	step_2,
+	step_3,
+	step_3,
+	step_3,
+	step_3,
+	step_3,
+	step_4,
+	step_5,
+	step_5,
+	step_6,
+	step_2,
+	step_3,
+	step_15,
+	step_16,
+	step_17,
+	step_18,
+	step_19,
 ]
 
 var current_step = 0
@@ -93,6 +133,7 @@ var level_over : bool = false
 var window_path : bool = false
 var current_height : int = 0
 var current_doom_height : int = -20
+var gate_height : int = 0
 
 func _recursive_calls(node: Node) -> void:
 	_connect_button_signals(node)
@@ -103,6 +144,9 @@ func _connect_button_signals(node: Node) -> void:
 	if node is Button:
 		if node.name.contains("Crumble"):
 			node.pressed.connect(_on_crumble_button_pressed)
+			return
+		if node.name.contains("Raise"):
+			node.pressed.connect(_on_raise_button_pressed)
 			return
 		if node.name.contains("WindowPath"):
 			node.pressed.connect(_on_window_path_button_pressed)
@@ -136,6 +180,23 @@ func _on_crumble_button_pressed() -> void:
 	level_over = true
 	current_container.show()
 	level_lost.emit()
+
+func _on_raise_button_pressed() -> void:
+	if level_over: return
+	gate_height += 1
+	gate_opening_button.custom_minimum_size.y += 8
+	if gate_height >= 52:
+		gate_situation_label.text = ""
+	elif gate_height >= 48:
+		gate_situation_label.text = "You can pass under the gate."
+		gate_opening_button.text = "Go under"
+		gate_opening_button.disabled = false
+	elif gate_height >= 32:
+		gate_situation_label.text = "You can almost pass under the gate."
+		gate_opening_button.text = "Almost there"
+	elif gate_height >= 16:
+		gate_situation_label.text = "If you get on your knees now,\nyou might not get back up."
+		gate_opening_button.text = "Still can't fit"
 
 func _on_window_path_button_pressed() -> void:
 	window_path = true
