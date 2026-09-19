b23ab7621e723b614deda2caaef9ce495d4455b4
Marek Belski <marek.belski@gmail.com>
2026-09-19 08:26:04 -0700

    Adds 3D torch labels to the room.

---
+++ scenes/game/levels/level_3d/torch_label_3d.gd
@@ -0,0 +1,17 @@
+extends Label3D
+
+const FIRE_STRING = "\\%s/
+I"
+
+var is_flicker : bool = false
+
+func _ready():
+	while is_inside_tree():
+		var random_time = randf()
+		await get_tree().create_timer(random_time).timeout
+		if is_flicker:
+			is_flicker = false
+			text = FIRE_STRING % "^"
+		else:
+			is_flicker = true
+			text = FIRE_STRING % "'"
