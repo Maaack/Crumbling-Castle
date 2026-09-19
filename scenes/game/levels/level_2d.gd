2b6d8138932dab36f25f2de50f068541cb8a3bb6
hatmix <67128187+hatmix@users.noreply.github.com>
2026-09-18 23:06:26 -0400

    Add gate rubble effect

--- scenes/game/levels/level_2d.gd
+++ scenes/game/levels/level_2d.gd
@@ -3,6 +3,7 @@ extends "level.gd"
 @export var gate_speed = 4.0
 @onready var lose_area_2d = %LoseArea2D
 @onready var gate = %Gate
+@onready var gate_wall_label: Label = $World/Tower/GateWall/Label
 @onready var interact_input_hint = %InteractInputHint
 @onready var jump_input_hint = %JumpInputHint
 @onready var move_right_input_hint = %MoveRightInputHint
@@ -11,6 +12,12 @@ extends "level.gd"
 @onready var second_checkpoint_marker_2d = %SecondCheckpointMarker2D
 @onready var third_checkpoint_marker_2d = %ThirdCheckpointMarker2D
 @onready var character_body_2d = %CharacterBody2D
+@onready var debris_right: GPUParticles2D = %DebrisRight
+@onready var debris_left: GPUParticles2D = %DebrisLeft
+@onready var dust_cloud: Node2D = %DustCloud
+@onready var dust_cloud_2: Node2D = %DustCloud2
+
+var _gate_shake_intensity: float = 0
 
 func _on_lose_area_2d_body_entered(_node: Node2D) -> void:
 	if _node.is_in_group(&"player"):
@@ -24,9 +31,21 @@ func _on_win_area_2d_body_entered(body: Node2D) -> void:
 
 func _process(delta):
 	lose_area_2d.position.y -= delta * doom_speed
+	var shake: Vector2 = Vector2(randf() - .5, randf() - .5) * _gate_shake_intensity
+	gate.label.offset_transform_position = shake
+	gate_wall_label.offset_transform_position = shake
+	debris_left.amount_ratio = clampf(_gate_shake_intensity, 0, 1)
+	debris_right.amount_ratio = clampf(_gate_shake_intensity, 0, 1)
+	_gate_shake_intensity = max(0, _gate_shake_intensity - delta * 10)
 
 func _on_gate_wheel_wheel_turned():
 	gate.position.y -= gate_speed
+	_gate_shake_intensity += 2
+	var tween: Tween = create_tween()
+	tween.tween_interval(2.0)
+	tween.tween_callback(dust_cloud.emit)
+	tween.tween_interval(0.5)
+	tween.tween_callback(dust_cloud_2.emit)
 
 func _on_gate_wheel_player_entered():
 	interact_input_hint.show()
