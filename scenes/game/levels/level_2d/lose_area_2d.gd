d33fecb058b06a603e510be87a887fc514797905
hatmix <67128187+hatmix@users.noreply.github.com>
2026-09-19 00:03:26 -0400

    Add walls destroy on lose_area height

--- scenes/game/levels/level_2d/lose_area_2d.gd
+++ scenes/game/levels/level_2d/lose_area_2d.gd
@@ -19,8 +19,8 @@ func _on_body_shape_entered(_body_rid: RID, body: Node2D, body_shape_index: int,
 	var body_shape_owner = body.shape_find_owner(body_shape_index)
 	var body_shape_node: CollisionShape2D = body.shape_owner_get_owner(body_shape_owner)
 
-	if body_shape_node is Wall:
-		body_shape_node.destroy_bottom_row()
+	#if body_shape_node is Wall:
+	#	body_shape_node.destroy_bottom_row()
 
 	if body_shape_node.get_parent() is StairStep:
 		body_shape_node.get_parent().destroy_bottom_row()
