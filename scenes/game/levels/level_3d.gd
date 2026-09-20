481cd40008f99d548ca529a8656c2d82914e6ce5
Marek Belski <marek.belski@gmail.com>
2026-09-20 00:36:24 -0700

    Adds interactable gate and gate wheel to 3D level.

--- scenes/game/levels/level_3d.gd
+++ scenes/game/levels/level_3d.gd
@@ -1,9 +1,12 @@
 extends "level.gd"
 
 @export var doom_speed : float = 3.6
+@export var gate_speed : float = 1.0
 
 @onready var lose_area_3d = %LoseArea3D
 @onready var character_body_3d = %CharacterBody3D
+@onready var interact_input_hint_3d = %InteractInputHint3D
+@onready var gate_3d = %Gate3D
 
 func _on_lose_area_3d_body_entered(_body: Node3D) -> void:
 	var tween = create_tween()
@@ -18,3 +21,17 @@ func _on_win_area_3d_body_entered(_body: Node3D) -> void:
 func _process(delta):
 	var doom_frame_offset = delta * doom_speed
 	lose_area_3d.position.y += doom_frame_offset
+
+func _on_gate_wheel_3d_player_entered():
+	interact_input_hint_3d.show()
+
+func _on_gate_wheel_3d_player_exited():
+	interact_input_hint_3d.hide()
+
+func _on_gate_wheel_3d_wheel_turned():
+	gate_3d.position.y += gate_speed
+	var tween: Tween = create_tween()
+	#tween.tween_interval(2.0)
+	#tween.tween_callback(dust_cloud.emit)
+	#tween.tween_interval(0.5)
+	#tween.tween_callback(dust_cloud_2.emit)
---
+++ scenes/game/levels/level_3d/gate_wheel_3d.gd
@@ -0,0 +1,50 @@
+extends Node3D
+
+signal wheel_turned
+signal player_entered
+signal player_exited
+
+enum WheelState{
+	CARDINAL,
+	DIAGONAL
+}
+
+@export var max_turns : int = 24
+@onready var animation_player = %AnimationPlayer
+
+var wheel_state : WheelState = WheelState.CARDINAL
+var can_interact : bool = false
+var turns : int = 0
+
+func _turn_wheel() -> void:
+	match wheel_state:
+		WheelState.CARDINAL:
+			wheel_state = WheelState.DIAGONAL
+			animation_player.play(&"diagonal")
+		WheelState.DIAGONAL:
+			wheel_state = WheelState.CARDINAL
+			animation_player.play(&"cardinal")
+	wheel_turned.emit()
+
+func turn_wheel() -> void:
+	if turns >= max_turns:
+		return
+	turns += 1
+	_turn_wheel()
+
+func _on_area_3d_body_entered(body : Node3D) -> void:
+	if body.is_in_group(&"player"):
+		can_interact = true
+		player_entered.emit()
+
+func _on_area_3d_body_exited(body : Node3D) -> void:
+	if body.is_in_group(&"player"):
+		can_interact = false
+		player_exited.emit()
+
+func _input(event : InputEvent) -> void:
+	if not can_interact:
+		return
+	if event.is_action_pressed(&"interact"):
+		turn_wheel()
+		
